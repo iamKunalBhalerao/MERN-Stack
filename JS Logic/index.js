a34150b9e2808d56fc14418eea0d476e179fb8ea
Kunal Bhalerao <kunalbhalerao789@gmail.com>
2024-10-08 08:51:18 +0530

    learned spread and rest operator

--- JS Logic/index.js
+++ JS Logic/index.js
@@ -1,11 +1,34 @@
-const names = ["Prem", "Kunal", "Tejas", "Sachin"];
+// const names = ["Prem", "Kunal", "Tejas", "Sachin"];
 
-// map function
-names.map((values) => {
-  return values;
-});
+// // map function
+// names.map((values) => {
+//   return values;
+// });
 
-// filter function
-names.filter((values) => {
-  return values.startsWith("K");
-});
+// // filter function
+// names.filter((values) => {
+//   return values.startsWith("K");
+// });
+
+// Spread operator
+
+// for copying array
+// const names = ["Prem", "Kunal", "Tejas", "Sachin"];
+// let copynames = [...names];
+
+// for copying object
+// const person = {
+//   name: "John",
+//   age: 30,
+//   city: "New York",
+// };
+
+// let copyobj = { ...person };
+
+// Rest operator
+
+// function abcd(a, b, c, ...args) {
+//   console.log(a, b, c, args);
+// }
+
+// abcd(1, 2, 3, 4, 5, 6);
